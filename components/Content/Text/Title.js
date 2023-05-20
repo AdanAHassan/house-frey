a282f1879e88552dd3127ea9f0f3a413827f3185
AdanAHassan <adan-a-hassan@hotmail.com>
2023-05-20 22:40:10 +0100

    Intro should not display a title

--- components/Content/Text/Title.js
+++ components/Content/Text/Title.js
@@ -8,7 +8,8 @@ import Subtitle from "./Subtitle.js"
 const Title = ({titleContent}) => {
     return (
         <div>
-            <h2 class="text-4xl mx-2 mt-4 mb-2" id={titleContent.id}>{titleContent.title}</h2>
+            {titleContent.id!=="Intro" &&
+                <h2 class="text-4xl mx-2 mt-4 mb-2" id={titleContent.id}>{titleContent.title}</h2>}
             {Array.isArray(titleContent.content) ? 
                 titleContent.content.map(item =>(
                     <Paragraph key={`${titleContent.content.id}-${titleContent.content.indexOf(item)}`} text={item}/>
